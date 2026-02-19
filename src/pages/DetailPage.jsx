9c8323fa396cf3581e35cfbda500f6331f9a6f6b
g_zawie <gzawie24@gmail.com>
2026-02-19 11:27:34 +0300

    Add LoginPage, MainPage, DetailPage with default exports

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DetailPage() {
+  return (
+    <div className="p-8 min-h-screen bg-black text-white">
+      Detail Page
+    </div>
+  );
+}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div className="p-8 min-h-screen bg-black text-white">
+      Main Page
+    </div>
+  );
+}
